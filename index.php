baf61b0dce72899bc24a817836972e86637e51e1
panachaiiiiiii <112429933+panachaiiiiiii@users.noreply.github.com>
2023-11-14 07:45:56 +0700

    insert form

--- index.php
+++ index.php
@@ -13,33 +13,45 @@
 
 </head>
     <body style="font-family: 'Mitr', sans-serif;">
-        <div class="grid grid-cols-2 gap-y-6 min-h-full sm:my-40 ">
-                <div class="my-20 col-span-2">
-                    <p class="text-4xl text-center">BOOM MARKET LOGIN</p>
-                </div>
+        <form action="db/checkuer.php" method="post">
+            <div class="grid grid-cols-2 gap-y-6 min-h-full sm:my-40 ">
+                    <div class="my-20 col-span-2">
+                        <p class="text-4xl text-center">BOOM MARKET LOGIN</p>
+                    </div>
 
-                <div class="mx-auto col-span-2">
-                    <p>Username</p>
-                    <input id="Username" name="Username" type="Username" autocomplete="email" required class="block  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 m:text-sm sm:leading-6">
-                </div>
+                    <div class="mx-auto col-span-2">
+                        <p>Username</p>
+                        <input id="Username" name="Username" type="text" autocomplete="email" required class="block  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 m:text-sm sm:leading-6">
+                    </div>
 
-                <div class="mx-auto col-span-2">
-                    <p>Password</p>
-                    <input id="Password" name="Password" type="Password" autocomplete="email" required class="block  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 m:text-sm sm:leading-6">
-                </div>
+                    <div class="mx-auto col-span-2">
+                        <p>Password</p>
+                        <input id="Password" name="Password" type="Password" autocomplete="email" required class="block  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 m:text-sm sm:leading-6">
+                    </div>
 
-                <div class="mx-auto col-span-2">
-                    <a href="" class="rounded-lg bg-sky-400 p-2">Login</a>
-                </div>
+                    <div class="mx-auto col-span-2">
+                        <!-- <a href="" type="submit" class="rounded-lg bg-sky-400 p-2">Login</a> -->
+                        <input type="submit" class="rounded-lg bg-sky-400 p-2" value="Submit">
+                    </div>
 
-                <div class="text-right">
-                    <p class="">สมัครตรงนี้จ้า</p>
-                </div>
-                
-                <div class="text-left mx-9">
-                    <a href="" class="rounded-lg bg-sky-400 p-1">Register</a>
-                </div>
+                    <div class="text-right">
+                        <p class="">สมัครตรงนี้จ้า -></p>
+                    </div>
+                    
+                    <div class="text-left mx-9">
+                        <a href="" class="rounded-lg bg-yellow-400 p-1">Register</a>
+                    </div>
 
-        </div>
+            </div>
+        </form>
     </body>
 </html>
+
+<!-- 
+git add .    
+git commit -m "first"
+git branch -M main
+git remote add origin https://github.com/panachaiiiiiii/marketboom.git
+git push - u origin main
+
+-->
